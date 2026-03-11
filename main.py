696234e2c16eca5c027442b9d42320bebfe25716
erikhh92 <erikhh92@gmail.com>
2026-03-11 16:27:42 +0100

    Working version

--- main.py
+++ main.py
@@ -1,34 +1,165 @@
 import requests
 import io
+import os
+import json
 from pypdf import PdfReader
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Configuración
-PDF_URL = "https://ejemplo.com/diario.pdf"
-KEYWORDS = ["palabra1", "palabra2"]
-TELEGRAM_TOKEN = "tu_token"
-TELEGRAM_CHAT_ID = "tu_id"
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URLS = os.getenv('URLS')
+KEYWORDS = os.getenv('KEYWORDS')
+ENV = os.getenv('ENV')
 
-def check_pdf():
+url_array = []
+keywords_array = []
+message_list = []
+
+def check_url(target):
     try:
-        response = requests.get(PDF_URL, timeout=10)
+        print(f"Checking URL: {target}")
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(target, headers=headers, timeout=15)
         response.raise_for_status()
-        
-        with io.BytesIO(response.content) as f:
+
+        scrappe_content(response)
+    except Exception as e:
+        print(f"Error checking the URL {target}: {e}")
+
+def scrappe_content(response: requests.Response):
+    try:
+        content_type = response.headers.get('Content-Type', '').lower()
+
+        if "application/pdf" in content_type:
+            scrappe_pdf(response.content)
+        elif "text/html" in content_type:
+            scrappe_html(response.text)
+        elif "application/json" in content_type:
+            scrappe_json(response.json())
+        else:
+            print(f"Content-type not recognised: {content_type}")
+                
+    except Exception as e:
+        print(f"Error while scrapping content: {e}")
+
+def scrappe_pdf(content):
+    try:
+        with io.BytesIO(content) as f:
             reader = PdfReader(f)
             text = "".join(page.extract_text() for page in reader.pages).lower()
             
-            found = [word for word in KEYWORDS if word.lower() in text]
+            found = [word for word in keywords_array if word.lower() in text]
             
             if found:
-                message = f"🎯 ¡Encontrado! El PDF contiene: {', '.join(found)}"
-                send_notification(message)
+                print(f"Found entry while scrapping PDF")
+                message = f"🎯 Found! The PDF contains: {', '.join(found)}"
+                save_message(message)
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error while scrapping PDF: {e}")
+
+
+def scrappe_html(content):
+    try:
+        soup = BeautifulSoup(content, 'html.parser')
+        
+        body = soup.find("body")
+
+        if body:
+            for trash in body(["script", "style", "nav", "footer", "header"]):
+                trash.decompose()
+
+            text = body.get_text()
+
+            found = [word for word in keywords_array if word.lower() in text]
+
+            if found:
+                print(f"Found entry while scrapping HTML")
+                message = f"🎯 Found! The HTML contains:: {', '.join(found)}"
+                save_message(message)
+    except Exception as e:
+        print(f"Error while scrapping HTML: {e}")
+    
+def scrappe_json(content):
+    try:
+        text = json.dumps(content, ensure_ascii=False)
+
+        found = [word for word in keywords_array if word.lower() in text]
+
+        if found:
+            print(f"Found entry while scrapping JSON")
+            message = f"🎯 Found! The JSON contains: {', '.join(found)}"
+            save_message(message)
+    except Exception as e:
+        print(f"Error while scrapping JSON: {e}")
+    
+def check_urls():
+    try:
+        for url in url_array:
+            check_url(url)
+    except Exception as e:
+        print(f"Error checking the URLs: {e}")
+
+def set_configuration():
+    global url_array, keywords_array
+    try:
+        url_array = URLS.split(';')
+        keywords_array = KEYWORDS.split(';')
+    except Exception as e:
+        print(f"Error setting the configuration: {e}")
+
+def save_message(msg):
+    try:
+        message_list.append(msg)
+    except Exception as e:
+        print(f"Error saving the message {msg}: {e}")
 
 def send_notification(msg):
-    # Ejemplo con Telegram, pero podrías usar un simple print o Webhook
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": msg})
+    try:
+        if ENV and ENV != "local":
+            print(f"Sending notifications through Telegram...")
+            url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+            requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+        else:
+            print(f"Sending notifications through terminal output...")
+            print(f"{msg}")
+    except Exception as e:
+        print(f"Error sending a notification {msg}: {e}")
+
+def send_telegram_notifications():
+    try:
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+        for message in message_list:
+            requests.post(url, data={"chat_id": CHAT_ID, "text": message})
+    except Exception as e:
+        print(f"Error sending Telegram notifications: {e}")
+
+def send_terminal_notifications():
+    try:
+        for message in message_list:
+            print(f"{message}")
+    except Exception as e:
+        print(f"Error sending terminal notifications: {e}")
+
+def send_notifications():
+    try:
+        if ENV and ENV != "local":
+            print(f"Sending notifications through Telegram...")
+            send_telegram_notifications()
+        else:
+            print(f"Sending notifications through terminal output...")
+            send_terminal_notifications()
+    except Exception as e:
+        print(f"Error sending the notifications: {e}")
 
 if __name__ == "__main__":
-    check_pdf()
+    try:
+        set_configuration()
+        check_urls()
+        send_notifications()
+    except Exception as e:
+        print(e)
